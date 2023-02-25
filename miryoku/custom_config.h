db6938d30c80e78ba03fab812e83c715618acdd5
Ben Christensen <ben@joinhandshake.com>
2023-02-25 09:14:07 -0700

    (Not tested) switch order of home rod mods to match osx layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+U_MT(LSHFT, A),     U_MT(LCTRL, S),     U_MT(LALT, D),    U_MT(LGUI, F),    &kp G,             &kp H,             U_MT(RGUI, J),    U_MT(RALT, K),    U_MT(RCTRL, L),     U_MT(RSHFT, SQT),   \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
